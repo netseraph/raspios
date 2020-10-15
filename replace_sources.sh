a55e9831a15380172f35baf849030e209620a8b9
netseraph <netseraph@gmail.com>
2020-10-15 17:52:12 +0800

    Update replace_sources.sh

--- replace_sources.sh
+++ replace_sources.sh
@@ -1,1 +1,2 @@
 sudo sed -i 's|raspbian.raspberrypi.org|mirrors.ustc.edu.cn/raspbian|g' /etc/apt/sources.list
+sudo sed -i 's|//archive.raspberrypi.org|//mirrors.ustc.edu.cn/archive.raspberrypi.org|g' /etc/apt/sources.list.d/raspi.list
